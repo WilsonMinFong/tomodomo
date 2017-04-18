746da50929b296c24dea02461c9d7b8447aa30b3
Wilson Fong <wilsonmfong@gmail.com>
2017-04-18 12:03:10 -0400

    Add backend components for user auth

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,24 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: ["Invalid email or password"], status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if logged_in?
+      logout!
+      render json: {}
+    else
+      render status: 404
+    end
+  end
+end
---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,18 @@
+class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :full_name, :password)
+  end
+end
