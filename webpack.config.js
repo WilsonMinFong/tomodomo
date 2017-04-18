6fb5718fb97de2cee6030957d7c952dd575f9ed2
Wilson Fong <wilsonmfong@gmail.com>
2017-04-18 12:18:47 -0400

    Add frontend skeleton and additional gems

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  entry: './frontend/tomodomo.jsx',
+  output: {
+    filename: './app/assets/javascripts/bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
