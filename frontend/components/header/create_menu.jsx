65d4589bf2c87d8a3ae2863a2cdc955420cf53e6
Wilson Fong <wilsonmfong@gmail.com>
2017-04-20 21:30:20 -0400

    Fix more menu styling

--- frontend/components/header/create_menu.jsx
+++ frontend/components/header/create_menu.jsx
@@ -4,11 +4,13 @@ import BoardFormContainer from '../boards/board_form_container';
 const CreateMenu = () => {
   return (
     <div className='create-menu'>
-      <span>Create a board...</span>
-      <div className='description'>Use a board to track a project or manage anything.</div>
       <BoardFormContainer/>
     </div>
   );
 };
 
 export default CreateMenu;
+
+// For expansion of menu
+// <span>Create a board...</span>
+// <div className='description'>Use a board to track a project or manage anything.</div>
