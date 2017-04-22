8268d2a6851622e3ca3370c58e7a05224a40960f
Wilson Fong <wilsonmfong@gmail.com>
2017-04-22 13:17:45 -0400

    Add reordering logic to list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,13 +5,16 @@
 #  id         :integer          not null, primary key
 #  name       :string           not null
 #  board_id   :integer          not null
-#  order      :integer          not null
+#  ord        :integer          not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 
 class List < ApplicationRecord
-  validates :name, :board, :order, presence: true
+  validates :name, :board, :ord, presence: true
+
+  before_validation :ensure_ord
+  before_update :update_ords!
 
   belongs_to :board
 
@@ -19,5 +22,36 @@ class List < ApplicationRecord
     through: :board,
     source: :creator
 
-  # List.where(board_id: 16).count
+  private
+
+  # add to end of list if not specified or outside range
+  def ensure_ord
+    next_ord = List.where(board_id: board_id).count
+    if ord.nil? || ord < 0 || ord > next_ord
+      self.ord = next_ord
+    end
+  end
+
+  # update other ords of other lists when reordering
+  def update_ords!
+    old_ord = List.find(id).ord
+
+    return nil if ord == old_ord
+
+    if ord < old_ord
+      List.where(
+        "board_id = ? AND ord >= ? AND ord < ?",
+        board_id,
+        ord,
+        old_ord
+      ).update_all("ord = ord + 1")
+    else
+      List.where(
+        "board_id = ? AND ord > ? AND ord <= ?",
+        board_id,
+        old_ord,
+        ord
+      ).update_all("ord = ord - 1")
+    end
+  end
 end
