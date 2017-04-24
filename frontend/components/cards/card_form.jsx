b09b6cd0c51411bb798b2618b460dcb83718faac
Wilson Fong <wilsonmfong@gmail.com>
2017-04-24 13:58:24 -0400

    Add card create form to cards index

---
+++ frontend/components/cards/card_form.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+
+class CardForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: props.card ? props.card.name : ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({ name: e.currentTarget.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    let card = Object.assign(
+      {},
+      this.state,
+      { list_id: this.props.listId }
+    );
+
+    if (this.props.card) {
+      card = Object.assign(card, { id: this.props.card.id });
+    }
+
+    this.props.formAction(card).then(
+      () => {
+        if (this.props.formType === 'new') {
+          this.props.removeAllPopovers();
+        }
+      }
+    );
+  }
+
+  handleKeyDown(e) {
+    if (e.key === 'Enter') {
+      this.handleSubmit(e);
+    }
+  }
+
+  render() {
+    const { formType } = this.props;
+
+    const createForm = (
+      <form onSubmit={ this.handleSubmit }>
+        <input
+          type='text'
+          value={ this.state.name }
+          onChange={ this.handleInput }/>
+
+        <input type='submit' value='Add' className='button save-card'/>
+      </form>
+    );
+
+    const updateInput = (
+      <input
+        type='text'
+        value={ this.state.name}
+        onChange={ this.handleInput }
+        onBlur={ this.handleSubmit }
+        onKeyDown={ this.handleKeyDown } />
+    );
+
+    return (
+      <div className='card-form'>
+        { formType === 'new' ? createForm : updateInput }
+      </div>
+    );
+  }
+}
+
+export default CardForm;
--- frontend/components/cards/cards_index_container.js
+++ frontend/components/cards/cards_index_container.js
@@ -1,17 +1,20 @@
 import { connect } from 'react-redux';
 import { fetchCards } from '../../actions/card_actions';
+import { receivePopover } from '../../actions/popover_actions';
 import { selectSortedListCards } from '../../reducers/selectors';
 import CardsIndex from './cards_index';
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    cards: selectSortedListCards(state, ownProps.listId)
+    cards: selectSortedListCards(state, ownProps.listId),
+    listId: ownProps.listId
   };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    fetchCards: () => dispatch(fetchCards(ownProps.listId))
+    fetchCards: () => dispatch(fetchCards(ownProps.listId)),
+    receivePopover: (popover) => dispatch(receivePopover(popover))
   };
 };
 
