2d72255722848eb92ea808fb098ec99df5fafb95
Wilson Fong <wilsonmfong@gmail.com>
2017-04-24 15:17:48 -0400

    Add styling for card create form

--- frontend/reducers/popover_reducer.js
+++ frontend/reducers/popover_reducer.js
@@ -3,7 +3,7 @@ import { RECEIVE_POPOVER, REMOVE_ALL_POPOVERS } from '../actions/popover_actions
 const popoverReducer = (oldState = '', action) => {
   switch (action.type) {
     case RECEIVE_POPOVER:
-      return oldState ? '' : action.popover;
+      return oldState === action.popover ? '' : action.popover;
     case REMOVE_ALL_POPOVERS:
       return '';
     default:
